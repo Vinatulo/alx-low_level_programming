8d1df49b3a63074ef3ce8c812d6acc6a6d7cd847
Vinatulo <vinatulo@gmail.com>
2023-02-16 12:13:06 -0800

    my first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print a code using puts
  * Return: is 0
@@ -8,6 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-	
+	return (0);
 }
