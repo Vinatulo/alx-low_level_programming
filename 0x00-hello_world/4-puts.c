6dddbe6278ef139f5db59490f6048598f3a0c474
Vinatulo <vinatulo@gmail.com>
2023-02-16 11:43:09 -0800

    puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print "Programming is like building a multilingual puzzle"
- * Return: This returns 0
+ * main - print a code using puts
+ * Return: is 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 	
 }
