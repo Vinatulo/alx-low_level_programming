e34396cd59fccb469d04d5bfe4938fd9d5f8abd9
Vinatulo <vinatulo@gmail.com>
2023-02-17 12:52:12 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char lo;
+
+	for (lo = 'a'; lo <= 'z'; lo++)
+	{
+		putchar(lo);
+	}
+	putchar('\n'0);
+	return (0);
+}
