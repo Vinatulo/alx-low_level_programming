16d4439c9289a88d569193f49d780a2c9c965a37
Vinatulo <vinatulo@gmail.com>
2023-02-21 18:26:48 -0800

    islower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabet
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char j;
+
+	for (j = 'a' ; j <= 'z' ; j++)
+		_putchar(j);
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_islower - function tp print lower case
+ *@c: parameter to be printed
+ *Return: 1 if lower case & 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
