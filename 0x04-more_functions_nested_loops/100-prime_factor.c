9e9b51d98236bd55c30e5631b098a166faabfa6c
Vinatulo <vinatulo@gmail.com>
2023-02-24 12:05:33 -0800

    prints the largest prime factor of the number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints the largest prime factor of the number
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long prime = 612852475143, div = 2;
+
+	while (div < (prime / 2))
+	{
+		prime /= 2;
+		continue;
+	}
+	for (div = 3; div < (prime / 2); div += 2)
+	{
+		if ((prime % div) == 0)
+		{
+			prime /= div;
+		}
+	}
+	printf("%ld\n", prime);
+	return (0);
+}
