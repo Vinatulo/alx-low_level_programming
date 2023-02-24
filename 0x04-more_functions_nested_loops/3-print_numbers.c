e813ab8c705e2f16476760a7959bd7897b2cb80b
Vinatulo <vinatulo@gmail.com>
2023-02-24 01:49:13 -0800

    numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * print_number - function that prints the number, from 0 to 9
+ * print_numbers - function that prints the number, from 0 to 9
  *
  * Return: 0-9 followed by new line
  */
-void print_number(void)
+void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+	{
+		_putchar((i % 10) + '0');
+	}
 	_putchar('\n');
 }
