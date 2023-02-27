0e4cca3773907cfbe34d16aebe282429183d1004
Vinatulo <vinatulo@gmail.com>
2023-02-27 11:37:05 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an int
+ * as parameter & update the value to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
