5e9603595bc1c869d0d64a55de0ab197b254d9f4
Vinatulo <vinatulo@gmail.com>
2023-02-27 15:35:02 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcpy - copies the string pointed to by src, 
+ * _strcpy - copies the string pointed to by src,
  * including the '\0' to the buffer pointed to by dest
  * @dest: pointer to the destination buffer
  * @src: pointer to the source string
