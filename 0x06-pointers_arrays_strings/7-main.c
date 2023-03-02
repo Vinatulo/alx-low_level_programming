fc6a71711f06743906451c0bb07bac1445146627
Vinatulo <vinatulo@gmail.com>
2023-03-02 03:58:30 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst.\n"
+	char s[] = "Expect the best. Prepare for the worst.\t"
 		"Capitalize on what comes.\n";
 	char *p;
 
